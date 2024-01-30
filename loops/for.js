80d70cc9af82171d394e7459dda9fd729fee5363
Tanha <nusrattanha897@gmail.com>
2024-01-31 02:40:01 +0600

    basics of for loop

---
+++ loops/for.js
@@ -0,0 +1,43 @@
+// for
+//syntax
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+// first step: initialization
+// second step: condition check. If true, code execution.
+// third step: increment
+// fourth step: condition check. If true, code execution.
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    // console.log(element);
+}
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if(element == 5){
+        //console.log("5 is best number");
+    }
+    //console.log(element);
+}
+
+
+//
+
+for(let i = 0; i <= 10; i++){
+    // console.log(`Outer loop value: ${i}`);
+    for(let j = 0; j <= 5; j++){
+        // console.log(`Inner loop value: ${j} and inner loop ${i}`);
+        
+    }
+}
+
+for(let i = 1; i<= 5; i++){
+    console.log(`Table of ${i}`);
+    for(let j = 1; j <= 10; j++){
+        console.log(`${i} * ${j} = ${i*j}`);
+    }
+}
+
+
+// console.log(element); //? ReferenceError:element is not defined
