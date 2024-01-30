625765a2041dd773ae92b7272968792088e21eeb
Tanha <nusrattanha897@gmail.com>
2024-01-30 23:01:48 +0600

    another example of switch

--- switch.js
+++ switch.js
@@ -7,6 +7,7 @@
 //         break;
 // }
 
+//info: if break is not used, after matching the key, the code is run and executed except default.
 const day = 3;
 switch (day) {
   case 1:
@@ -33,4 +34,47 @@ switch (day) {
 
   default:
     console.log("Please enter a valid number");
+}
+
+const month = 'march';
+switch(month){
+    case 'jan':
+        console.log('January');
+        break;
+    case 'feb':
+        console.log('February');
+        break;
+    case 'march':
+        console.log('March');
+        break;
+    case 'april':
+        console.log('April');
+        break;
+    case 'may':
+        console.log('May');
+        break;
+    case 'june':
+        console.log('June');
+        break;
+    case 'july':
+        console.log('July');
+        break;
+    case 'aug':
+        console.log('August');
+        break;
+    case 'sep':
+        console.log('September');
+        break;
+    case 'oct':
+        console.log('October');
+        break;
+    case 'nov':
+        console.log('November');
+        break;
+    case 'dec':
+        console.log('December');
+        break;
+    default:
+        console.log('Invalid Input');
+        
 }
